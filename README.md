b67f723d38660fb9ac20e29278f7ff44ce3f4c7c
David Portella <david@davidportella.cl>
2016-06-03 15:58:58 -0400

    doc(core) - Add details for config in README and env variables override with parameters in the execution

--- README.md
+++ README.md
@@ -8,6 +8,22 @@ To map ports private servers through the public server.
 
 Do not forget to create the file .config with your own data (example in config.example).
 
+#### Config ####
+
+In this file environment variables are set for the execution of varBash
+
+* **USER="username"** Username for public host
+* **PUBLIC_HOST="public.host.name"** Hostname or IP for public server
+* **PRIVATE_HOST="private.host.name"** Hostname or IP for private server
+* **PORTS="ports separated by commas"** Ports separeted by commas, example "80 443 8080"
+
+#### Args ####
+
+Running parameters is overwritten with the value of the corresponding environment variables
+
+* **-u | --user** Replace environment variable USER 
+* **-b | --private-host** Replace environment variable PRIVATE_HOST
+
 ### Who do I talk to? ###
 
 david@davidportella.cl
--- tunneling.sh
+++ tunneling.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+unsetEnv() {
+  echo Unset variables
+  unset USER
+  unset PUBLIC_HOST
+  unset PRIVATE_HOST
+  unset PORTS
+}
+
+createTunnel() {
+  /usr/bin/ssh -f $USER@$PUBLIC_HOST -L $1:$PRIVATE_HOST:$1 -N > /dev/null 2>&1
+  if [[ $? -eq 0 ]]; then
+    echo Tunnel $1 created successfully
+  else
+    echo An error occurred creating a tunnel $?
+  fi
+}
+
+echo "Using file config .config environment variable"
 if [ -f .config ]; then
   source .config
+  if [ $# -gt 0 ]; then
+    echo "Using command line arguments ( $# )"
+    while [[ "$#" > 1 ]]; do case $1 in
+      -u|--user) USER="$2";;
+      -b|--private-host) PRIVATE_HOST="$2";;
+      *);;
+    esac; shift
+    done
+  fi
 else
   echo The configuration file does not exist
   exit
@@ -16,18 +43,10 @@ else
     ssh-copy-id $USER@$PUBLIC_HOST
   else
     echo Do not exist id_rsa.pub, can create one with next command "ssh-keygen -t rsa"
+    unsetEnv
     exit
   fi
 fi
-
-createTunnel() {
-  /usr/bin/ssh -f $USER@$PUBLIC_HOST -L $1:$PRIVATE_HOST:$1 -N > /dev/null 2>&1
-  if [[ $? -eq 0 ]]; then
-    echo Tunnel $1 created successfully
-  else
-    echo An error occurred creating a tunnel $?
-  fi
-}
 
 ports=($PORTS)
 for i in ${ports[@]}; do
@@ -38,3 +57,5 @@ for i in ${ports[@]}; do
     echo Tunnel ${i} exist
   fi
 done
+
+unsetEnv
